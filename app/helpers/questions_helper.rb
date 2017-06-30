09a17b907c794598c3077ce29e5ffd7135011eed
R. ABHINAV <abhinavramesh@hotmail.com>
2017-06-30 22:46:01 +0300

    Refactored code for question bookmarks using helpers

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,11 @@
 module QuestionsHelper
+
+  def question_bookmark_helper(question)
+    if current_user.bookmark_question?(question)
+      render partial: 'questions/unbookmark', locals: { question: question }
+    else
+      render partial: 'questions/bookmark', locals: { question: question }
+    end
+  end
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,16 +3,16 @@ module SessionsHelper
 	def log_in(user)
 		session[:user_id] = user.id
 	end
-	
-	
+
+
 	def logged_in?
 		!current_user.nil?
 	end
-	
+
 	def current_user?(user)
 		user == current_user
 	end
-	
+
 	def current_user
   	if(user_id = session[:user_id])  #checks whether session of user_id exists or not
   		@current_user ||= User.find_by(id: user_id)
@@ -24,39 +24,39 @@ module SessionsHelper
   		end
   	end
   end
-  
+
   def forget(user)
   	#user.forget
   	cookies.delete(:user_id)
   	cookies.delete(:remember_token)
   end
-  
+
   def log_out
   	forget(current_user)
   	session.delete(:user_id)
   	@current_user = nil
   end
-  
+
   def remember(user)
   	user.remember
   	cookies.permanent.signed[:user_id] = user.id
   	cookies.permanent[:remember_token] = user.remember_token
   end
-  
+
   def forget(user)
   	user.forget
  		cookies.delete(:user_id)
  		cookies.delete(:remember_token)
  	end
-  
+
    # Redirects to stored location (or to the default)
   def redirect_back_or(default)
   	redirect_to(session[:forwarding_url] || default)
   	session.delete(:forwarding_url)
   end
-  
+
   def store_location
   	session[:forwarding_url] = request.original_url if request.get?
   end
-  
+
 end
