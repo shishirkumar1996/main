fc3dd9f216f03dd75f991b9e1ed03b359f755d95
R. ABHINAV <abhinavramesh@hotmail.com>
2017-06-30 22:51:32 +0300

    Design of Answer and Bookmark buttons for questions and general styling for the question-box

--- app/controllers/question_bookmark_relations_controller.rb
+++ app/controllers/question_bookmark_relations_controller.rb
@@ -5,11 +5,10 @@ before_action :logged_in_user
 		@question = Question.find(params[:question_id])
 		current_user.bookmarked_questions << @question
 	end
-	
+
 	def destroy
 		@question = QuestionBookmarkRelation.find(params[:id]).question
 		current_user.bookmarked_questions.delete(@question)
 	end
-
 
 end
