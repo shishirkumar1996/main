fa59a1a6fd0d13b68577610542586bbd3e2e6c38
R. ABHINAV <abhinavramesh@hotmail.com>
2017-07-03 22:46:33 +0300

    Changed question partial to make question-box design global

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,14 @@
 class StaticPagesController < ApplicationController
 	before_action :logged_in_user	,except: [:home]
-	
-	
+
+
 	def home
-	
+
 		if logged_in?
 			require 'will_paginate/array'
 			@articles = Article.all
 			@questions = Question.all
-			@feed = (@articles+@questions).sort_by(&:created_at).reverse.paginate(page: params[:page],per_page: 2)
+			@feed = (@articles+@questions).sort_by(&:created_at).reverse.paginate(page: params[:page],per_page: 6)
 			respond_to do |format|
 				format.js
 				format.html
@@ -19,7 +19,7 @@ class StaticPagesController < ApplicationController
 	def dummy
 	respond_to do |format|
 	format.js
-	end	
+	end
 	end
 	def check_existing
 		respond_to do |format|
@@ -27,14 +27,14 @@ class StaticPagesController < ApplicationController
 		format.html
 		end
 	end
-	
+
 	def search_question
 		@value = Question.search(params[:term]).to_a.map{|x| {id: x.id,text: x.title}}
 		respond_to do |format|
 			format.json{	render json: @value}
 			format.html
 		end
-		
+
 	end
-	
+
 end
