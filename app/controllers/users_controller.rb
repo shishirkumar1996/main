90b676ce1f8a2e2225e9ab98bd9d7d992eb7d728
R. ABHINAV <abhinavramesh@hotmail.com>
2017-07-04 11:31:32 +0300

    Fixed error with index action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,16 @@ class UsersController < ApplicationController
 	before_action :same_user, only: [:edit_image,:edit_password,:edit_name,:update_name,:update_password,
 	:edit,:bookmarks]
 	def index
-	@users = User.where("name ILIKE ?","%#{params[:term]}%").map{|user| {:id=>user.id,:text =>user.name}}
-	
-	respond_to do |format|
-		format.json {
-			render :json =>@users
+		@users = User.where("name ILIKE ?","%#{params[:term]}%").map{|user| {:id=>user.id,:text =>user.name}}
+
+		respond_to do |format|
+			format.json {
+				render :json =>@users
 			}
+			format.html
 		end
 	end
-	
+
 	def academic
 		@user = User.find(params[:id])
 		respond_to do |format|
@@ -20,7 +21,7 @@ class UsersController < ApplicationController
 			format.html
 		end
 	end
-	
+
 	def prepopulateacademic
 		@academics = current_user.institutes.map{|institute| { :id => institute.id, :text => institute.name}}
 		respond_to do |format|
@@ -28,9 +29,9 @@ class UsersController < ApplicationController
 				render :json => @academics
 				}
 			end
-		
+
 	end
-	
+
 	def addacademic
 		@id = params[:academic]
 		if(@id[0..2] == "<<<")
@@ -55,15 +56,15 @@ class UsersController < ApplicationController
 			format.html
 		end
 	end
-	
+
 	def removeacademic
 		@academic = Institute.find(params[:academic])
 		current_user.universities.delete(@academic)
 	end
-	
-	
-	
-	
+
+
+
+
 	def interest
 		@user = User.find(params[:id])
 		respond_to do |format|
@@ -71,9 +72,9 @@ class UsersController < ApplicationController
 			format.html
 		end
 	end
-	
-	
-	
+
+
+
 	def prepopulateinterest
 		@interests = current_user.relations.map{|interest| { :id => interest.id, :text => interest.name}}
 		respond_to do |format|
@@ -81,10 +82,10 @@ class UsersController < ApplicationController
 				render :json => @interests
 				}
 			end
-		
+
 	end
-	
-	
+
+
 	def addinterest
 		@id = params[:interest]
 		if(@id[0..2] == "<<<")
@@ -100,53 +101,53 @@ class UsersController < ApplicationController
 			format.html
 		end
 	end
-	
-	
+
+
 	def removeinterest
 		@interest = Domain.find(params[:interest])
 		current_user.relations.delete(@interest)
 	end
-	
+
 	def edit_image
 	end
-	
-	
+
+
 	def image
 		current_user.image = params[:image]
 		current_user.save!
 		redirect_to edit_image_user_path(current_user)
 	end
-	
+
 	def following
 #		@title = "Following"
 		@user = User.find(params[:id])
 		@users = @user.following.all
 	end
-	
+
 	def followers
 #		@title = "Followers"
 		@user = User.find(params[:id])
 		@users = @user.followers.all
 	end
-	
+
 	def bookmarks
 		@user = User.find(params[:id])
 		@bookmarks = (@user.bookmarked_questions+@user.bookmarked_articles)
 	end
-	
+
 	def about
 		@user = User.find(params[:id])
 	end
-	
-	
+
+
 	def index_group
 		@groups = current_user.groups
 	end
-	
+
 	def edit
 		@user = User.find(params[:id])
-	end	
-	
+	end
+
 	def edit_name
 		@user= User.find(params[:id])
 			respond_to do |format|
@@ -154,14 +155,14 @@ class UsersController < ApplicationController
 				format.html
 			end
 	end
-	
+
 	def edit_password
 		@user = User.find(params[:id])
 	end
-	
+
 	def update_name
-		@user = User.find(params[:id])	
-		
+		@user = User.find(params[:id])
+
 		 if @user.update_attributes(user_params)
 		 	flash[:success] = "name updated"
 			redirect_to user_path(@user)
@@ -169,9 +170,9 @@ class UsersController < ApplicationController
 			flash[:danger] = "some error occured"
 				redirect_to edit_user_path(@user)
 		end
-	
+
 	end
-	
+
 	def update_password
 		@user = User.find(params[:id])
 		if @user.authenticate(params[:user][:old_password]) &&
@@ -183,12 +184,12 @@ class UsersController < ApplicationController
 		redirect_to edit_password_user_path(@user)
 		end
 	end
-	
-	
+
+
 	def new
 		@user = User.new
 	end
-	
+
 	def create
 		@user = User.new(user_params)
 		if @user.save
@@ -198,14 +199,14 @@ class UsersController < ApplicationController
 			render 'new'
 		end
 	end
-	
+
 	def show
 		require 'will_paginate/array'
 		@user = User.find(params[:id])
 		@useranswer = []
 		@user.answers.each do |answer|
 			@useranswer << answer.question
-		end   
+		end
 		#answers of different questions to be added in the feed
 		@feed = (@user.articles+@useranswer).sort_by(&:created_at).reverse.paginate(page: params[:page],per_page: 2)
 		respond_to do |format|
@@ -213,29 +214,29 @@ class UsersController < ApplicationController
 			format.js
 		end
 	end
-	
+
 	def update
 	end
-		
-	
+
+
 	def write
 		respond_to do |format|
 			format.js
 			format.html
 		end
 	end
-	
+
 private
 
 	def user_params
 		params.require(:user).permit(:name,:email,:password,:password_confirmation,:remote_image_url)
 	end
-	
+
 	def same_user
 		unless(current_user == User.find(params[:id]))
 			redirect_to root_url
 		end
 	end
-	
-	
+
+
 end
