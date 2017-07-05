187cc57e415c5dfe0d9ee4d8112585826380c020
R. ABHINAV <abhinavramesh@hotmail.com>
2017-07-05 12:33:59 +0300

    Custom date-time helper created and integrated into question partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def date_time_helper(date_time)
+    current_time = Time.zone.now
+    days = (current_time - date_time).to_i / 1.day
+    if(days > 31)
+      "on #{date_time.strftime("%b %d, %Y")}"
+    else
+      "#{distance_of_time_in_words(date_time, current_time)} ago"
+    end
+  end
+  
 end
