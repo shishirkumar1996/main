1a474984b310c4979a8d7395bf59e039260ff05f
R. ABHINAV <abhinavramesh@hotmail.com>
2017-07-05 14:46:22 +0300

    Created helper for article bookmarks, designed a basic article-box and made the button styles generic

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,11 @@
 module ArticlesHelper
+
+  def article_bookmark_helper(article)
+    if current_user.bookmark_article?(article)
+      render partial: 'articles/unbookmark', locals: { article: article }
+    else
+      render partial: 'articles/bookmark', locals: { article: article }
+    end
+  end
+
 end
