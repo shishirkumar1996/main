171d61d07dbd9cb0b0a22323ade4adc45d2d9b68
R. ABHINAV <abhinavramesh@hotmail.com>
2017-07-10 17:48:23 +0300

    Modified date-time helper and incorporated the changes into questions and articles partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,10 @@ module ApplicationHelper
     current_time = Time.zone.now
     days = (current_time - date_time).to_i / 1.day
     if(days > 31)
-      "on #{date_time.strftime("%b %d, %Y")}"
+      "#{date_time.strftime("%b %d, %Y")}"
     else
-      "#{distance_of_time_in_words(date_time, current_time)} ago"
+      "#{distance_of_time_in_words(date_time, current_time)} ago".capitalize
     end
   end
-  
+
 end
