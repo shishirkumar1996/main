ad553456f2c2651162c48819a093c442a4b1416d
R. ABHINAV <abhinavramesh@hotmail.com>
2017-08-06 10:35:47 +0530

    Basic fading notification for question bookmark creation

--- app/controllers/question_bookmark_relations_controller.rb
+++ app/controllers/question_bookmark_relations_controller.rb
@@ -4,11 +4,19 @@ before_action :logged_in_user
 	def create
 		@question = Question.find(params[:question_id])
 		current_user.bookmarked_questions << @question
+		respond_to do |format|
+			format.js { flash.now[:notice] = 'Question bookmark successfully created.' }
+		end
+
 	end
 
 	def destroy
 		@question = QuestionBookmarkRelation.find(params[:id]).question
 		current_user.bookmarked_questions.delete(@question)
+		respond_to do |format|
+			format.js { flash.now[:notice] = 'Question bookmark successfully deleted.' }
+		end
+
 	end
 
 end
