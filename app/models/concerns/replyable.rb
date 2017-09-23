cf72cd79a9c1fcf079e1ae8c5fc877d516ee79d2
R. ABHINAV <abhinavramesh@hotmail.com>
2017-09-23 23:00:31 +0530

    Wrote a callback for replies and sanitized the article reply body

--- app/models/concerns/replyable.rb
+++ app/models/concerns/replyable.rb
@@ -1,12 +1,26 @@
 module Replyable
   extend ActiveSupport::Concern
 
-  VALID_BODY_REGEX = /\A(?!(&nbsp;|<p>|<\/p>|\s)*\z).+/
+  VALID_BODY_REGEX = /[a-zA-Z0-9]/        # /\A(?!(&nbsp;|<p>|<\/p>|\s)*\z).+/
 
   included do
     belongs_to :user
     default_scope -> { order(created_at: :desc) }
     validates :body, presence: true, format: {with: VALID_BODY_REGEX}
+    before_save :remove_whitespace
   end
+
+  private
+
+    def remove_whitespace
+      unless body.nil?
+        body.strip!
+        unless body.empty?
+          body.prepend '<p>'
+          body << '</p>'
+          body.gsub! /\s+/, '</p><p>'
+        end
+      end
+    end
 
 end
