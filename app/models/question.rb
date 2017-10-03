ddb9e2e268b2623640ae9ab0a46e93764b2e589b
R. ABHINAV <abhinavramesh@hotmail.com>
2017-10-03 17:21:01 +0530

    Added search_data method to the Question model and also the ability to highlight and suggest question title searches

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
-	
-	searchkick word_start: [:title]
+
+	searchkick word_start: [:title], highlight: [:title], suggest: [:title]
 
 	has_many :question_bookmark_relations,foreign_key: :question_id,
 	dependent: :destroy
@@ -12,9 +12,17 @@ class Question < ApplicationRecord
 	has_many :domains, through: :domains_questions
 	has_many :answers  , dependent: :destroy
 	validates :title, presence: true
-	
+
 	def domain_tokens=(tokens)
 		self.group_ids = Domain.ids_from_tokens(tokens)
 	end
-	
+
+	# Beware of anonymous, description and other attributes. If u want to include below, reindex
+	def search_data
+		{
+			title: title,
+			created_at: created_at,
+			user_id: user_id
+		}
+	end
 end
