ab6ececca20342b2f52b67ab292ceb342e142eeb
R. ABHINAV <abhinavramesh@hotmail.com>
2017-11-08 18:58:51 +0530

    Name validation, a callback and some model tests for groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,12 +1,13 @@
 class Group < ApplicationRecord
 
+	before_validation { name.strip! }
+	validates :name, presence: true, length: { maximum: 50 }
+	attr_reader :user_tokens
 	mount_uploader :image, GroupimageUploader
 	has_many :group_articles, dependent: :destroy
 	has_many  :group_questions,dependent: :destroy
 	has_many :groups_users, foreign_key: :group_id, dependent: :destroy
 	has_many :members, through: :groups_users, source: :user
-
-	attr_reader :user_tokens
 
 	def user_tokens=(tokens)
 		self.user_ids = User.ids_from_tokens(tokens)
