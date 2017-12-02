e8f84bea5589f6072f6907c8ed6f9c04f64e9312
R. ABHINAV <abhinavramesh@hotmail.com>
2017-12-02 18:31:47 +0300

    Added uniqueness validations and removed unnecessary foreign key references in the 3 bookmark models

--- app/models/answer_bookmark_relation.rb
+++ app/models/answer_bookmark_relation.rb
@@ -1,8 +1,8 @@
 class AnswerBookmarkRelation < ApplicationRecord
 	self.table_name = 'answer_bookmarks'
-	belongs_to :user, foreign_key: :user_id
-	belongs_to :answer,foreign_key: :answer_id
-	validates :user_id,presence: true
+	belongs_to :user
+	belongs_to :answer
+	validates :user_id, presence: true
 	validates :answer_id, presence: true
-
+	validates :user_id, uniqueness: { scope: :answer_id }
 end
--- app/models/question_bookmark_relation.rb
+++ app/models/question_bookmark_relation.rb
@@ -1,8 +1,8 @@
 class QuestionBookmarkRelation < ApplicationRecord
 	self.table_name = 'question_bookmarks'
-	belongs_to :user,foreign_key: :user_id
-	belongs_to :question, foreign_key: :question_id
-	validates :user_id,presence: true
+	belongs_to :user
+	belongs_to :question
+	validates :user_id, presence: true
 	validates :question_id, presence: true
-
+	validates :user_id, uniqueness: { scope: :question_id }
 end
