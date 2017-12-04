b0173cebfd858bfd2de6f643fd5bba28e3cd0d63
R. ABHINAV <abhinavramesh@hotmail.com>
2017-12-04 19:20:51 +0300

    Changed type_name of an entity to underscored version of its class instead of downcased version

--- app/models/concerns/writable.rb
+++ app/models/concerns/writable.rb
@@ -2,7 +2,7 @@ module Writable
   extend ActiveSupport::Concern
   included do
     self.class_eval do
-      class_name = self.name.downcase
+      class_name = self.name.underscore
       define_method("type_name") do
         class_name
       end
