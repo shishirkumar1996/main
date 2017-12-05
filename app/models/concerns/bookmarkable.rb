58a736b3479d31767ba7d5de147b6c4299c789a2
R. ABHINAV <abhinavramesh@hotmail.com>
2017-12-05 17:50:15 +0300

    Replaced downcase with underscore in bookmarkable

--- app/models/concerns/bookmarkable.rb
+++ app/models/concerns/bookmarkable.rb
@@ -4,7 +4,7 @@ module Bookmarkable
     self.class_eval do
       class_plural = self.name.pluralize
       has_many :bookmarks, class_name: "#{class_plural}::Bookmark"
-      bookmarked_entities = "bookmarked_#{class_plural.downcase!}".to_sym
+      bookmarked_entities = "bookmarked_#{class_plural.underscore!}".to_sym
       define_method(:bookmarked?) do |current_user|
         current_user.public_send(bookmarked_entities).include? self
       end
