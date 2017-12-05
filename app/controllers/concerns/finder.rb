2b3fb852bbefbf39f19435323f7b796d007d6f10
R. ABHINAV <abhinavramesh@hotmail.com>
2017-12-05 11:23:07 +0300

    Changing order in switch case in finder concern

--- app/controllers/concerns/finder.rb
+++ app/controllers/concerns/finder.rb
@@ -3,12 +3,12 @@ module Finder
   private
     def find_by_type(id, type)
       case type
-      when 'question'
-        Question.select(:id).find(id)
       when 'article'
         Article.select(:id).find(id)
       when 'answer'
         Answer.select(:id).find(id)
       end
+      when 'question'
+        Question.select(:id).find(id)
     end
 end
