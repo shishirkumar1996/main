a4f209027aaf8ae91f70f9e84119a7c550b080aa
R. ABHINAV <abhinavramesh@hotmail.com>
2017-12-06 22:00:11 +0300

    Fixed semantic errors in bookmarkable module

--- app/models/concerns/bookmarkable.rb
+++ app/models/concerns/bookmarkable.rb
@@ -4,7 +4,8 @@ module Bookmarkable
     self.class_eval do
       class_plural = self.name.pluralize
       has_many :bookmarks, class_name: "#{class_plural}::Bookmark"
-      bookmarked_entities = "bookmarked_#{class_plural.underscore!}".to_sym
+      class_plural = class_plural.underscore
+      bookmarked_entities = "bookmarked_#{class_plural}".to_sym
       define_method(:bookmarked?) do |current_user|
         current_user.public_send(bookmarked_entities).include? self
       end
