41f636ea713c546d9dcd344316b4bdc229413b14
R. ABHINAV <abhinavramesh@hotmail.com>
2017-12-19 16:46:27 +0300

    Removed the old upvote and downvote associations from article and answer models

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,16 +5,6 @@ class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :question, counter_cache: true
   has_many :answer_replies , dependent: :destroy
-  has_many :answerrelations,foreign_key: :answer_id,dependent: :destroy
-  has_many :likes, through: :answerrelations, source: :user
-
-  has_many :badanswerrelations,foreign_key: :answer_id,
-  dependent: :destroy
-  has_many :dislikes,through: :badanswerrelations,source: :user
-
-
   VALID_BODY_REGEX = /\A(?!(&nbsp;|<p>|<\/p>|\s)*\z).+/
-
   validates :body,presence: true,format: {with: VALID_BODY_REGEX}
-
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -4,22 +4,11 @@ class Article < ApplicationRecord
 	include Votable
 	searchkick word_start: [:title]
 	belongs_to :user
-
 	has_many :article_replies, dependent: :destroy
 	has_many :domains_articles, foreign_key: "article_id",dependent: :destroy
 	has_many :domains, through: :domains_articles
-
-	has_many :articlerelations,foreign_key: :article_id,
-	dependent: :destroy
-	has_many :likes, through: :articlerelations,source: :user
-
-	has_many :badarticlerelations,foreign_key: :article_id,
-	dependent: :destroy
-	has_many :dislikes, through: :badarticlerelations,source: :user
-
 	validates :title,presence: true
 	VALID_BODY_REGEX = /\A(?!(&nbsp;|<p>|<\/p>|\s)*\z).+/
-
 	validates :body,presence: true,format: {with: VALID_BODY_REGEX}
 
 	def domain_tokens=(tokens)
