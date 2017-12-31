e12e0f4510e0fc9eb2868376f1755b231e3b065a
R. ABHINAV <abhinavramesh@hotmail.com>
2017-12-31 18:44:13 +0300

    Made the ManyToManyRelatable concern general by improving finding of association and table names from module and class names

--- app/models/concerns/many_to_many_relatable.rb
+++ app/models/concerns/many_to_many_relatable.rb
@@ -2,13 +2,17 @@ module ManyToManyRelatable
   extend ActiveSupport::Concern
   included do
     self.class_eval do
-      klass_split = self.name.split '::'
-      klass = klass_split[0].singularize
+      class_split = self.name.split '::'
+      class_name_plural = class_split.pop.underscore.pluralize
+      module_name_singular = "::#{class_split.join.singularize}"
+      assoc_name = class_split.map!{|x| x.singularize.underscore}.join('_').to_sym
+      assoc_name_id = "#{assoc_name}_id".to_sym
+      self.table_name = "#{assoc_name}_#{class_name_plural}"
+      belongs_to assoc_name, class_name: module_name_singular
       belongs_to :user, class_name: '::User'
-      klass_underscore = klass.underscore.to_sym
-      self.table_name = "#{klass_underscore}_#{klass_split[1].underscore.pluralize}"
-      belongs_to klass_underscore, class_name: "::#{klass}"
-      validates :user_id, uniqueness: { scope: "#{klass_underscore}_id".to_sym }
+      validates :user_id, presence: true
+      validates assoc_name_id, presence: true
+      validates :user_id, uniqueness: { scope: assoc_name_id }
     end
   end
 end
