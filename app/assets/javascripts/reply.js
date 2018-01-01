d5840a85531802a5edf9c72d70bdf81d5fd98d0c
R. ABHINAV <abhinavramesh@hotmail.com>
2018-01-01 17:19:56 +0300

    JS functions to help with replies creation and display

--- app/assets/javascripts/reply.js
+++ app/assets/javascripts/reply.js
@@ -1,3 +1,35 @@
+$.fn.scrollView = function () {
+  return this.each(function () {
+    $('html, body').animate({
+      scrollTop: $(this).offset().top
+    }, 1000);
+  });
+}
+
+function getLevel(parent) {
+  var lastLetter, classLen, classList = parent.attr('class').split(/\s+/);
+  for(var i = 0; i < classList.length; i++) {
+    if(classList[i].slice(-7, -2) == "level") {
+      classLen = classList[i].length;
+      lastLetter = parseInt(classList[i].charAt(classLen - 1));
+      if(!isNaN(lastLetter)) return (lastLetter + 1);
+    }
+  }
+  return 0;
+}
+
+function resetSubmitReply() {
+  var parentType = $('input[name="parent_type"]');
+  if(parentType.val().slice(-6) == "_reply") {
+    parentType.val(parentType.val().slice(0, -6));
+    var parentID = $('input[name="parent_id"]');
+    parentID.val(parentType.parent().parent().attr('id').split('_').pop());
+    $('.reply-header').hide();
+    $('.cancel-reply').hide();
+    $('.comment-header').show();
+  }
+}
+
 //$(function(){
 $(document).on('turbolinks:load',function(){
 	$(document).on('click','.answerreply_loadmore',function(){
@@ -56,4 +88,25 @@ $(document).on('turbolinks:load',function(){
 			});
 		}
 	});
+
+	$(document).on('click', '.reply-to-reply a', function(event) {
+		event.preventDefault();
+    var replyButtonContainer = $(this).parent();
+		var parentReplyID = replyButtonContainer.children('input[name="parent_reply_id"]').val();
+		$('input[name="parent_id"]').val(parentReplyID);
+		var parentType = $('input[name="parent_type"]');
+    if(parentType.val().slice(-6) != "_reply") parentType.val(parentType.val() + '_reply');
+    var replyTo = replyButtonContainer.parent().find('.author-name a').text();
+    $('.reply-header').text('Reply to ' + replyTo);
+    $('.comment-header').hide();
+    $('.reply-header').show();
+    $('.cancel-reply').show();
+		$('.comment-header').scrollView();
+		$('.submit-article-reply textarea').focus();
+	});
+
+  $(document).on('click', '.cancel-reply a', function(event) {
+    event.preventDefault();
+    resetSubmitReply();
+  });
 });
