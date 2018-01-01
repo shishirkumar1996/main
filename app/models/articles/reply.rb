85b6827d10f9d6c165ea9793523f9c189bf0244d
R. ABHINAV <abhinavramesh@hotmail.com>
2018-01-01 17:05:37 +0300

    Moved the ArticleReply model to the articles namespace

---
+++ app/models/articles/reply.rb
@@ -0,0 +1,11 @@
+module Articles
+  class Reply < ApplicationRecord
+    include Writable
+    attr_accessor :level
+    self.table_name = 'article_replies'
+    belongs_to :user, class_name: '::User'
+    belongs_to :article, class_name: '::Article'
+    has_many :replies, class_name: "#{self.name}", foreign_key: 'parent_reply_id'
+    belongs_to :parent_reply, class_name: "#{self.name}"
+  end
+end
