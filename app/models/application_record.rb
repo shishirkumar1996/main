93e2c601cfa2609cc755d0c65b66a7a6bd725c95
R. ABHINAV <abhinavramesh@hotmail.com>
2018-01-08 10:14:31 +0530

    A class method to define associations and constraints on many-to-many models

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,22 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
+  def self.initialize_many_to_many(assoc_one, assoc_two)
+    [assoc_one, assoc_two].each_with_index do |assoc, index|
+      klass = assoc[0]
+      assoc_name = (assoc.length == 1 ? klass.underscored_name : assoc[1]).to_sym
+      assoc_id = "#{assoc_name}_id".to_sym
+      belongs_to assoc_name, class_name: "::#{klass.name}"
+      validates assoc_id, presence: true
+      if index == 0
+        assoc_one_id = assoc_id
+      else
+        assoc_two_id = assoc_id
+      end
+    end
+    validates assoc_one_id, uniqueness: { scope: assoc_two_id }
+  end
+
   def type_name
     self.class.underscored_name
   end
