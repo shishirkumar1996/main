dcd1ef0d810e6f834d18724f0da12babc910e4d0
R. ABHINAV <abhinavramesh@hotmail.com>
2018-01-09 09:15:34 +0530

    Moved many_to_many_initialize from application_record to many_to_many_relatable

--- app/models/concerns/many_to_many_relatable.rb
+++ app/models/concerns/many_to_many_relatable.rb
@@ -1,18 +1,20 @@
 module ManyToManyRelatable
   extend ActiveSupport::Concern
-  included do
-    self.class_eval do
-      class_split = self.name.split '::'
-      class_name_plural = class_split.pop.underscore.pluralize
-      module_name_singular = "::#{class_split.join.singularize}"
-      assoc_name = class_split.map!{|x| x.singularize.underscore}.join('_').to_sym
-      assoc_name_id = "#{assoc_name}_id".to_sym
-      self.table_name = "#{assoc_name}_#{class_name_plural}"
-      belongs_to assoc_name, class_name: module_name_singular
-      belongs_to :user, class_name: '::User'
-      validates :user_id, presence: true
-      validates assoc_name_id, presence: true
-      validates :user_id, uniqueness: { scope: assoc_name_id }
+
+  def initialize_many_to_many(assoc_one, assoc_two)
+    assoc_one_id = assoc_two_id = nil
+    [assoc_one, assoc_two].each_with_index do |assoc, index|
+      klass = assoc[0]
+      assoc_name = (assoc.length == 1 ? klass.underscored_name : assoc[1]).to_sym
+      assoc_id = "#{assoc_name}_id".to_sym
+      belongs_to assoc_name, class_name: "::#{klass.name}"
+      validates assoc_id, presence: true
+      if index == 0
+        assoc_one_id = assoc_id
+      else
+        assoc_two_id = assoc_id
+      end
     end
+    validates assoc_one_id, uniqueness: { scope: assoc_two_id }
   end
 end
