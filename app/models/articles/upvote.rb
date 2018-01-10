4acc853a47e937c2e05d28d78acc385cd9ddfcae
R. ABHINAV <abhinavramesh@hotmail.com>
2018-01-10 14:21:25 +0530

    Moved the upvote model for articles to the articles namespace

--- app/models/articles/upvote.rb
+++ app/models/articles/upvote.rb
@@ -1,5 +1,15 @@
 module Articles
   class Upvote < ApplicationRecord
-  	include ManyToManyRelatable
+  	extend ManyToManyRelatable
+    self.table_name = underscored_name.pluralize
+    initialize_many_to_many [::User], [::Article]
+    validate :user_is_not_author
+=begin
+    def user_is_not_author
+      unless self.user_id.nil? && self.article_id.nil?
+        self.errors.add :base, 'Author cannot upvote' if self.user_id == self.article.user_id
+      end
+    end
+=end
   end
 end
