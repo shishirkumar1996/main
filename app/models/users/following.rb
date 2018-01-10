6a376875e4bb8e9369cfe890223f620c8f4b555b
R. ABHINAV <abhinavramesh@hotmail.com>
2018-01-10 14:19:37 +0530

    Moved the Users::Following class to the users namespace and added a new instance method for returning notification message

--- app/models/users/following.rb
+++ app/models/users/following.rb
@@ -3,5 +3,10 @@ module Users
     extend ManyToManyRelatable
     self.table_name = underscored_name.pluralize
     initialize_many_to_many [::User, 'followed'], [::User, 'follower']
+
+    def notif_msg
+  		follower_name = User.select(:name).find(follower_id).name
+  		"#{follower_name} is following you"
+  	end
   end
 end
